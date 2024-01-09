193a72cf0459c1926b440896adb1a9c8f49c8ec7
Quentin PAYET <quentinpayet1203@gmail.com>
2024-01-10 01:24:13 +0400

    search/{keyword} route added

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from typing import Union
-
+import io
 from fastapi import FastAPI
 
+import sqlite3
+from sqlite3 import Cursor
+
+from starlette.responses import StreamingResponse
+
 app = FastAPI()
+
+# TODO: Clean and refactor
+# TODO: Setup tests
+# TODO: Gérer les cas d'erreurs
+
+
+# TODO: Put in an other file
+def get_page(key_word: str, cur: Cursor) -> list[str]:
+    sql = ""
+    with open("./db/sql/select_pages.sql") as sql_select:
+        sql = sql_select.read()
+        sql_select.close()
+
+    res = cur.execute(sql, (f"%{key_word}%",))  # TODO: Put sql in a sql file
+    return res.fetchall()
 
 
 @app.get("/")
@@ -10,6 +29,12 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/search/{key_word}")
+def read_item(key_word: str):
+    con = sqlite3.connect("./db/db_v0.sqlite")
+    cur = con.cursor()
+
+    response = get_page(key_word, cur)
+
+    # return {"key_word": key_word, "first_text_content": response[0][0]}
+    return StreamingResponse(io.BytesIO(response[0][1]), media_type="image/jpg")
