f1a786e7a05dfad5353b93f2e800aeeddf34a1fb
Quentin PAYET <quentinpayet1203@gmail.com>
2024-01-20 16:11:19 +0400

    wip: new files downloaded and sql_utils.get_sql_statement() added and create_db.py cleaned

--- db/create_db.py
+++ db/create_db.py
@@ -1,10 +1,12 @@
 import pathlib
 import sqlite3
+import sql_utils
 
 path = pathlib.Path(__file__).parent.resolve()
 
 con = sqlite3.connect(f"{path}/db_v0.sqlite")
 cur = con.cursor()
 
-with open(f"{path}/sql/create_tables.sql") as create_tables:
-    cur.execute(create_tables.read())
+sql_statement = sql_utils.get_sql_statement("create_tables.sql")
+
+cur.execute(sql_statement)
---
+++ db/sql_utils.py
@@ -0,0 +1,12 @@
+import pathlib
+
+path = pathlib.Path(__file__).parent.resolve()
+
+
+def get_sql_statement(file_name: str) -> str:
+    sql_statement = ""
+    with open(f"{path}/sql/{file_name}") as sql_file:
+        sql_statement = sql_file.read()
+        sql_file.close()
+
+    return sql_statement
