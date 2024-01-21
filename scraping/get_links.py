81fc513cb7a06aa32b3155ded6b71874f4db69f7
Quentin PAYET <quentinpayet1203@gmail.com>
2024-01-21 12:07:43 +0400

    new links added

--- scraping/get_links.py
+++ scraping/get_links.py
@@ -90,7 +90,7 @@ refs = get_filtered_refs()
 
 print("\n-----Fetch file names from web-----")
 prospectus = get_all_files_names(
-    refs[:60], sleep=15
+    refs[:60], sleep=5
 )  # ! Change to the full refs list to get alls
 
 print("\n-----Remove low quality file names-----")
