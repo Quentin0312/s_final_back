c7ad96f0430bcb0fbc7be6f513fef35b3e9f3443
Quentin PAYET <quentinpayet1203@gmail.com>
2024-01-24 17:26:32 +0400

    data_labelizer.py done

--- data_labelizer.py
+++ data_labelizer.py
@@ -1,5 +1,3 @@
-import sys
-import time
 import cv2
 import sqlite3
 from db import sql_utils
@@ -7,11 +5,11 @@ from db import sql_utils
 """
 Labelisation des pages présents dans la database de training.
 
-Boucle
-    - image_path récup depuis db
-    - image à afficher
-    - input à enregistrer
-    - label à save dans un fichiers texte
+- Récupération de 'image_path' de n image_path depuis la db
+
+Boucle (chaque 'image_path')
+    - Affichage de l'image
+    - Label input de l'utilisateur enregistré dans un fichiers .txt
 """
 
 # TODO: Database de training à mettre en place !
@@ -26,27 +24,53 @@ Format:
 list[(id_page:int, image_path: str)]
 """
 
-# def loop():
-#     while cv2.waitKey(30) == ord("a")
+# ! Filter response of image that already have labels (check labels.txt) !
+
+
+# TODO: Put in utils file
+def get_label_from_key_pressed(key: int) -> int:
+    key_label_mapping = {
+        ord("d"): 0,
+        ord("e"): 1,
+        ord("s"): 2,
+        ord("a"): 3,
+        ord("p"): 4,
+        ord("t"): 5,
+        ord("i"): 6,
+        ord("h"): 7,
+        ord("b"): 8,
+        ord("c"): 9,
+        ord("m"): 10,
+        ord("s"): 11,
+        ord("."): 12,
+        ord("q"): 13,
+        ord("v"): 14,
+        ord("j"): 15,
+    }
+
+    return key_label_mapping[key]
+
 
 for item in response:
     id_page = item[0]
     image_path = item[1]
 
+    # Display image
     img = cv2.imread(image_path, cv2.IMREAD_ANYCOLOR)
 
     cv2.namedWindow(image_path, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(image_path, width=700, height=1000)
     cv2.imshow(image_path, img)
-    label = []
-    while True:
-        key_pressed = cv2.waitKey(30000)
-        if key_pressed == ord("\r"):  # `enter` key
-            break
-        else:
-            print("in else")
-            label.append(key_pressed)
-    # ! Mettre en place une fonction pour transfomer `label` en int de 0 à 15
-    # ! Afficher l'image plus petit
+
+    # Get label inputed
+    key_pressed = cv2.waitKey(0)
+
+    label = get_label_from_key_pressed(key_pressed)
+
     print("label", label)
     cv2.destroyAllWindows()
+
+    # Save in file
+    with open("./labels.txt", "a") as labels_file:
+        labels_file.write(image_path + "|" + str(label) + "\n")
+        labels_file.close()
