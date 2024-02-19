fd4ea9cc14601a2a8e8b03c05505b5859187063f
Quentin PAYET <quentinpayet1203@gmail.com>
2024-02-19 14:26:45 +0400

    categories insertion added to create_db.py

--- db/create_db.py
+++ db/create_db.py
@@ -11,6 +11,36 @@ cur = con.cursor()
 
 # Create tables
 sql_statement = sql_utils.get_sql_statement("create_tables_prod.sql")
-
-# cur.execute(sql_statement)
 cur.executescript(sql_statement)
+
+
+# Insert categories
+labels = [
+    (0, "Aménagement intérieur"),
+    (1, "Électroménager"),
+    (2, "Multimédia (son, TV)"),
+    (3, "Aménagement extérieur"),
+    (4, "Papeterie"),
+    (5, "Téléphone"),
+    (6, "Informatique"),
+    (7, "Grandes distribution"),
+    (8, "Bricolage / Bâtiments / Travaux"),
+    (9, "Culture (livres, jeux-vidéos, musique)"),
+    (10, "Mode / Vêtements / Chaussures"),
+    (11, "Sport"),
+    (12, "Bébé"),
+    (13, "Beauté / Bien être"),
+    (14, "Mobilité"),
+    (16, "Jouets"),
+]
+
+sql_insert_categories = sql_utils.get_sql_statement("insert_category_db_prod.sql")
+for label in labels:
+    cur.execute(
+        sql_insert_categories,
+        (
+            label[0],
+            label[1],
+        ),
+    )
+con.commit()
