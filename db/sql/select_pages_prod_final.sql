d9b22e6a11da26d8591691f25f5c534bad1a9b61
Quentin PAYET <quentinpayet1203@gmail.com>
2024-03-23 20:15:31 +0400

    route updated => multiple key word

---
+++ db/sql/select_pages_prod_final.sql
@@ -0,0 +1,11 @@
+SELECT
+    image_path,
+    "Catalog".start_date,
+    "Catalog".end_date,
+    Page_categories.id_category
+FROM
+    Page
+    JOIN "Catalog" ON Page.id_catalog = "Catalog".id_catalog
+    JOIN Page_categories ON Page_categories.id_page = Page.id_page
+WHERE
+    text LIKE ?
--- main.py
+++ main.py
@@ -33,17 +33,36 @@ app.add_middleware(
 
 # TODO: Rename
 # TODO: Put in an other file
+# def get_pages(key_words: list[str], category: int, cur: Cursor) -> list[str]:
+#     sql = sql_utils.get_sql_statement("select_pages_v1.sql")
+#     keyword = f"%{key_words[0]}%"
+
+#     # TODO: Take into account multiple key_words
+
+#     # Take into account the category
+#     if category != -1:
+#         sql += f" AND id_category = {category};"
+
+#     res = cur.execute(sql, (keyword,))
+#     return res.fetchall()
+
+
 def get_pages(key_words: list[str], category: int, cur: Cursor) -> list[str]:
-    sql = sql_utils.get_sql_statement("select_pages_v1.sql")
-    keyword = f"%{key_words[0]}%"
+    sql = sql_utils.get_sql_statement("select_pages_prod_final.sql")
+    first_keyword = f"%{key_words[0]}%"
 
     # TODO: Take into account multiple key_words
 
     # Take into account the category
     if category != -1:
-        sql += f" AND id_category = {category};"
+        sql += f" AND id_category = {category}"
 
-    res = cur.execute(sql, (keyword,))
+    if len(key_words) > 1:
+        for key_word in key_words:
+            if key_word != key_words[0]:
+                sql += f' AND text LIKE "%{key_word}%"'
+
+    res = cur.execute(sql, (first_keyword,))
     return res.fetchall()
 
 
@@ -68,21 +87,42 @@ class SearchInfos(BaseModel):
     category: int
 
 
+# @app.post("/search")
+# def read_item(search_infos: SearchInfos):
+#     # con = sqlite3.connect("./db/db_training_v1.sqlite")
+#     con = sqlite3.connect("./db/db_v1.sqlite")
+#     cur = con.cursor()
+
+#     response = get_pages(search_infos.key_words, search_infos.category, cur)
+#     print(response)
+
+#     # Linked categories
+#     categories = list(set([elt[2] for elt in response]))
+#     if None in categories:
+#         categories.remove(None)
+
+#     # Images
+#     images_encoded = [get_image_encoded(elt[1]) for elt in response]
+
+#     return {"categories": categories, "list_image_base64": images_encoded}
+
+
 @app.post("/search")
 def read_item(search_infos: SearchInfos):
-    # con = sqlite3.connect("./db/db_training_v1.sqlite")
-    con = sqlite3.connect("./db/db_v1.sqlite")
+    con = sqlite3.connect("./db/db_prod_final.sqlite")
     cur = con.cursor()
 
     response = get_pages(search_infos.key_words, search_infos.category, cur)
     print(response)
 
     # Linked categories
-    categories = list(set([elt[2] for elt in response]))
-    if None in categories:
-        categories.remove(None)
+    categories = list(set([elt[3] for elt in response]))
+    # if None in categories:
+    #     categories.remove(None)
 
+    # TODO: Specifier les dates
+    # TODO: Filtrer selon les dates
     # Images
-    images_encoded = [get_image_encoded(elt[1]) for elt in response]
+    images_encoded = [get_image_encoded(elt[0]) for elt in response]
 
     return {"categories": categories, "list_image_base64": images_encoded}
